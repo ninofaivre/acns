92b7eeea48da8365e9c43bfbc0ff0e034e384df7
nino <nino.faivre@netc.fr>
2025-06-18 17:05:27 +0200

    replaced nix/utils/genZon by nix2zon flake

--- flake.nix
+++ flake.nix
@@ -3,15 +3,17 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.05";
+    nix2zon.url = "github:ninofaivre/nix2zon";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nix2zon }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       inherit (nixpkgs) lib;
       zigDeps = import ./nix/deps.nix { inherit (pkgs) fetchFromGitHub; };
       version = "0.0.1";
+      inherit (nix2zon.lib) toZon;
     in {
       packages.${system}.acns = pkgs.stdenv.mkDerivation {
         meta = {
@@ -39,13 +41,15 @@
           ${lib.strings.concatMapStrings (value: ''
             ln -s ${value} ./deps/${lib.removePrefix "/nix/store/" value}
           '')  (lib.attrsets.attrValues zigDeps)}
-          >build.zig.zon cat <<< '${(import ./nix/utils/genZon.nix { inherit lib; }) {
-            name = "acns";
+          >build.zig.zon cat <<< '${toZon { value = {
+            name = ".acns";
             fingerprint = "0xda3d5caca4187a84";
             inherit version;
             paths = [ "src" "build.zig" ];
-            deps = zigDeps;
-          }}'
+            dependencies = builtins.mapAttrs (name: value: {
+              path = "\\./deps/${lib.removePrefix "/nix/store/" value}/";
+            }) zigDeps;
+          }; }}'
         '';
       };
 
